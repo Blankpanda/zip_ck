7f5b720051fd5190bff198a237ec92e5782e5794
Blankpanda <Blankpanda8@gmail.com>
2015-10-03 23:44:16 -0400

    worked on logger.cs

---
+++ zipck/zipck/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zipck
+{
+    class Logger
+    {
+
+        private static string Filename = "log.text";
+
+        public static void Write(string information)
+        {
+            // todo: order this in a list ?
+            // todo: if the log file for this session doesn't exist create it using the DateTime
+            Update(information);
+
+
+        }
+        
+        private  static void Update(string information)
+        {
+            // todo: we want to create one per session
+            WriteToTextFile(information);
+        }
+
+        private static void WriteToTextFile(string information)
+        {
+            using (System.IO.StreamWriter s = new System.IO.StreamWriter(Filename))
+            {
+                s.WriteLine(information);
+            }
+        }
+
+    }
+}
